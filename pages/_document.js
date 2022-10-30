c9230f4900f8900884dfee3604eceb3bc454298a
tamago0224 <bigtreesasuke200733@gmail.com>
2022-10-30 23:27:34 +0900

    ポートフォリオのCSS

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css?family=M+PLUS+1p"
+					rel="stylesheet"
+				/>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Head>
+		</Html>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -2,42 +2,74 @@ import { GitHub, Twitter } from "@mui/icons-material";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Tamago Portfolio</title>
+				<title>tamago0224@narberal-gamma.com</title>
+				<meta name="description" content="Tamago Portfolio site" />
 			</Head>
 
-			<div className="portfolio-container">
-				<h1>Tamago Portfolio</h1>
-				<div className="portfolio-avatar-img">
+			<div className={styles["portfolio-container"]}>
+				<h1>tamago0224@narberal-gamma.com</h1>
+				<div className={styles["portfolio-avatar-img"]}>
 					<Image
-						src="/images/portfolio-avatar.jpg"
+						src="/images/portfolio-avatar.png"
 						alt="tamago avatar"
 						width={100}
 						height={100}
 						style={{ borderRadius: "50px" }}
 					/>
 				</div>
-				<h2>About me</h2>
-				<p>Hi, I'm software developer.</p>
+				<h2>Abount Me</h2>
+				<p>都内でソフトウェアエンジニアとして働いています。</p>
 
-				<div className="portfolio-sns">
-					<h2>Social medias</h2>
+				<div className={styles["portfolio-sns"]}>
+					<h2>Social Medias</h2>
 					<Link href="https://github.com/tamago0224">
-						<GitHub />
+						<GitHub fontSize="large" sx={{ mx: "10px" }} />
 					</Link>
 					<Link href="https://twitter.com/tamago_0224">
-						<Twitter />
+						<Twitter fontSize="large" sx={{ mx: "10px" }} />
 					</Link>
+					<h2>Blogs</h2>
+					<ul>
+						<li>
+							<a
+								href="https://tamago0224.hatenablog.com"
+								target="_blank"
+								rel="noreferrer"
+							>
+								はてなブログ
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://zenn.dev/tamago0224"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Zenn
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://qiita.com/tamago_0224"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Qiita (あまり更新していない)
+							</a>
+						</li>
+					</ul>
 				</div>
 
-				<div className="portfolio-skill">
+				<div className={styles["portfolio-skill"]}>
 					<h2>Skills</h2>
 
-					<div className="portfolio-skill-card">
+					<div className={styles["portfolio-skill-card"]}>
 						<h3>Programing Language</h3>
 						<ul>
 							<li>Java (8/11)</li>
@@ -46,7 +78,7 @@ export default function Home() {
 						</ul>
 					</div>
 
-					<div className="portfolio-skill-card">
+					<div className={styles["portfolio-skill-card"]}>
 						<h3>Framework</h3>
 						<ul>
 							<li>Spring Boot</li>
@@ -54,14 +86,14 @@ export default function Home() {
 						</ul>
 					</div>
 
-					<div className="portfolio-skill-card">
+					<div className={styles["portfolio-skill-card"]}>
 						<h3>Provisioning Tools</h3>
 						<ul>
 							<li>Ansible</li>
 						</ul>
 					</div>
 
-					<div className="portfolio-skill-card">
+					<div className={styles["portfolio-skill-card"]}>
 						<h3>Tools</h3>
 						<ul>
 							<li>Git/Github</li>
@@ -71,7 +103,7 @@ export default function Home() {
 						</ul>
 					</div>
 
-					<div className="portfolio-skill-card">
+					<div className={styles["portfolio-skill-card"]}>
 						<h3>Cloud</h3>
 						<p>None</p>
 					</div>
